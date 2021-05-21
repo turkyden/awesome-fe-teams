17beb11988636dee47e0de2fe9f6aed96d112a6c
Turkyden <wj871287@gmail.com>
2021-05-21 16:26:41 +0800

    fixed: api with index

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,9 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const { username = 'Turkyden' } = request.query;
   try {
     const res = await axios.get(`https://api.github.com/users/${username}`);
-    response.status(200).json(res.data);
+    if (res.status === 200) {
+      response.status(200).json(res.data);
+    }
   } catch (error) {
     response.status(200).send(error.message);
   }
