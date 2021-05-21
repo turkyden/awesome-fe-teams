5f53395b06680024679379384aefb481e87b6f18
Turkyden <wj871287@gmail.com>
2021-05-21 15:20:38 +0800

    fixed the avatar info

--- api/hello.ts
+++ api/hello.ts
@@ -5,7 +5,7 @@ export default async (request: VercelRequest, response: VercelResponse) => {
   const { username = 'Turkyden' } = request.query;
   try {
     const res = await axios.get(`https://api.github.com/users/${username}`);
-    response.status(200).send(res);
+    response.status(200).json(res);
   } catch (error) {
     response.status(200).send(error.message);
   }
