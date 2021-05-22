45eae322d6f3da51decd8312fea1f4bc4005cc3d
Turkyden <wj871287@gmail.com>
2021-05-22 11:53:02 +0800

    fix: 🐛 fixed the star rank of gh api

--- api/index.ts
+++ api/index.ts
@@ -2,6 +2,11 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import axios from 'axios';
 import mineType from 'mime-types';
 
+const GITHUB_AUTH = {
+  username: 'dcb39fc858755f895513',
+  password: '803b0587d11fa32bc2aa553e4827ecb55c9c58fe',
+};
+
 const getBase64ByURL = async (url) => {
   try {
     const response = await axios.get(url, { responseType: 'arraybuffer' });
@@ -17,9 +22,15 @@ const getBase64ByURL = async (url) => {
  * @param param0
  * @returns
  */
-export const generateSVG = async ({ login, name, location, avatar_url }) => {
+export const generateSVG = async ({
+  login,
+  name,
+  location,
+  avatar_url,
+  public_repos,
+}) => {
   return `
-  <svg xmlns="http://www.w3.org/2000/svg" width="290" height="120" viewBox="0 0 290 120" fill="none">
+  <svg xmlns="http://www.w3.org/2000/svg" width="280" height="120" viewBox="0 0 280 120" fill="none">
     <style>
       .header {
         font: 600 18px 'Segoe UI', Ubuntu, Sans-Serif;
@@ -67,7 +78,7 @@ export const generateSVG = async ({ login, name, location, avatar_url }) => {
 
     <rect class="stagger" style="animation-delay: 900ms" data-testid="card-bg" x="0.5" y="0.5" rx="2" height="90%" stroke="#E5E7EB" width="270" fill="#fffefe" stroke-opacity="1" />
 
-    <g data-testid="main-card-body" transform="translate(65, 4)">
+    <g data-testid="main-card-body" transform="translate(64, 0)">
 
       <g transform="translate(-48, 14)" >
         <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:svgjs="http://svgjs.com/svgjs" version="1.1" width="64" height="64">
@@ -99,7 +110,10 @@ export const generateSVG = async ({ login, name, location, avatar_url }) => {
             <svg aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="16" height="16" class="light">
               <path fill-rule="evenodd" d="M8 .25a.75.75 0 01.673.418l1.882 3.815 4.21.612a.75.75 0 01.416 1.279l-3.046 2.97.719 4.192a.75.75 0 01-1.088.791L8 12.347l-3.766 1.98a.75.75 0 01-1.088-.79l.72-4.194L.818 6.374a.75.75 0 01.416-1.28l4.21-.611L7.327.668A.75.75 0 018 .25zm0 2.445L6.615 5.5a.75.75 0 01-.564.41l-3.097.45 2.24 2.184a.75.75 0 01.216.664l-.528 3.084 2.769-1.456a.75.75 0 01.698 0l2.77 1.456-.53-3.084a.75.75 0 01.216-.664l2.24-2.183-3.096-.45a.75.75 0 01-.564-.41L8 2.694v.001z"/>
             </svg>
-            <text class="stat light" x="20" y="12.5">14,000</text>
+            <text class="stat light" x="20" y="12.5">${await getTotalStar(
+              login,
+              public_repos,
+            )}</text>
           </g>
         </g>
         <g transform="translate(0, 65)">
@@ -116,19 +130,41 @@ export const generateSVG = async ({ login, name, location, avatar_url }) => {
   `;
 };
 
+export const getTotalStar = async (username: string, public_repos: number) => {
+  const pageSize = 30;
+  const pages = Math.ceil(public_repos / pageSize);
+  const promises = Array.from(new Array(pages), (v, i) => {
+    return new Promise((resolve, reject) => {
+      axios
+        .get(`https://api.github.com/users/${username}/repos?page=${i + 1}`, {
+          auth: GITHUB_AUTH,
+        })
+        .then(({ status, data }) => {
+          if (status === 200) {
+            const value: number = data.reduce(
+              (prev, next) => prev + (next.fork ? 0 : next.stargazers_count),
+              0,
+            );
+            resolve(value);
+          } else {
+            reject(0);
+          }
+        });
+    });
+  });
+  const chunks = await Promise.all(promises);
+  return chunks.reduce((prev: number, next: number) => prev + next, 0);
+};
+
 export default async (request: VercelRequest, response: VercelResponse) => {
   const { username = 'Turkyden' } = request.query;
   try {
     const res = await axios.get(`https://api.github.com/users/${username}`, {
-      auth: {
-        username: 'dcb39fc858755f895513',
-        password: '803b0587d11fa32bc2aa553e4827ecb55c9c58fe',
-      },
+      auth: GITHUB_AUTH,
     });
 
     if (res.status === 200) {
-      // response.status(200).json(res.data);
-      // const { avatar_url, login, name, location, blog, email } = res.data;
+      //response.status(200).json(res.data);
       const svg = await generateSVG(res.data);
       response.setHeader('Content-Type', 'image/svg+xml;charset=UTF-8');
       response.status(200).send(svg);
